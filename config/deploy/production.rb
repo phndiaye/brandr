bd853501c7cfab7fe7190b36b3c3d24e1941e043
Philippe Ndiaye <phndiaye@gmail.com>
2016-01-25 14:15:15 +0100

    Listen on tcp 8080 port

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -24,7 +24,7 @@ set :use_sudo,        true
 set :stage,           :production
 set :deploy_via,      :remote_cache
 set :deploy_to,       "/var/www/#{fetch(:application)}"
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_bind,       "tcp://0.0.0.0:8080"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
