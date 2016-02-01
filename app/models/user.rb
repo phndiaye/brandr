057a8b306a85fe67a2190ea5eafccc351f3add94
Philippe Ndiaye <phndiaye@gmail.com>
2016-02-01 17:40:12 +0100

    use *_of format for validation on multiple fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,5 @@ class User < ActiveRecord::Base
   has_many :hunts
 
   validates_presence_of :firstname, :lastname, :username, :email, :password
-  validates :email, :username, uniqueness: true
-  #validates_uniqueness_of :email, :username
+  validates_uniqueness_of :email, :username
 end
