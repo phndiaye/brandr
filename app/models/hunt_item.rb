7e0348f270d277c4efe42fc8f43b6782ff739c9d
Philippe Ndiaye <phndiaye@gmail.com>
2016-02-20 18:11:00 +0100

    The number of comments on the hunt will be computed client-side

--- app/models/hunt_item.rb
+++ app/models/hunt_item.rb
@@ -3,8 +3,4 @@ class HuntItem < ActiveRecord::Base
   has_many :hunt_comments
 
   validates_presence_of :top, :left
-
-  def comments_count
-    hunt_comments.count || 0
-  end
 end
--- app/serializers/hunt_item_serializer.rb
+++ app/serializers/hunt_item_serializer.rb
@@ -1,5 +1,5 @@
 class HuntItemSerializer < ActiveModel::Serializer
-  attributes :id, :top, :left, :comments_count, :created_at, :updated_at
+  attributes :id, :top, :left, :created_at, :updated_at
 
   has_many :hunt_comments
 end
